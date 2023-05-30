9a152672169bd180c1ad4c56049d6150da3fdc4a
Your Name Main <you@example.com>
2023-05-30 06:07:58 -0700

    Swiper js added user Review

--- src/Pages/Home/Testimonials/Feedback.js
+++ src/Pages/Home/Testimonials/Feedback.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Feedback = () => {
     return (
@@ -19,9 +25,56 @@ const Feedback = () => {
              <p className="p-2 text-sm font-medium tracking-wider text-center uppercase">TESTIMONIALS</p>
 	 	      <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">CLIENTS FEEDBACKS</h2>
 
-        <div>
+         <Swiper
+                loop={true}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div id="item1" className="flex flex-col w-full p-10 lg:p-20 text-center">
+                        <p className="text-gray-300 text-center font-semibold mt-4">Photography is a way of felling, of touching, of loving. What you have caught on film is capture forever... is remembers little things after have forgotten everything. Wild life Photography powerful medium expression and communication, offers an infinite variety of perception, interpretation</p>
 
-        </div>
+                        <div className=" mt-4">
+                            <img src="https://demo.voidcoders.com/htmldemo/potoliaV2/main-files/assets/img/avatar/t-avatar-1.jpg" className='rounded-xl mx-auto' alt="" />
+                            <div className="ml-3 text-gray-300">
+                                <h5 className="text-xl font-semibold">Olivia  Martin</h5>
+                                <p className="font-semibold">Guest review</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+               {/* 2 */}
+             <SwiperSlide>
+                    <div id="item1" className="flex flex-col w-full p-10 lg:p-20 text-center">
+                        <p className="text-gray-300 text-center font-semibold mt-4">Photography is a way of felling, of touching, of loving. What you have caught on film is capture forever... is remembers little things after have forgotten everything. Wild life Photography powerful medium expression and communication, offers an infinite variety of perception, interpretation</p>
+
+                        <div className=" mt-4">
+                            <img src="https://demo.voidcoders.com/htmldemo/potoliaV2/main-files/assets/img/avatar/t-avatar-2.jpg" className='rounded-xl mx-auto' alt="" />
+                            <div className="ml-3 text-gray-300">
+                                <h5 className="text-xl font-semibold">Olivia  Martin</h5>
+                                <p className="font-semibold">Guest review</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+               {/* 3 */}
+              <SwiperSlide>
+                    <div id="item1" className="flex flex-col w-full p-10 lg:p-20 text-center">
+                        <p className="text-gray-300 text-center font-semibold mt-4">Photography is a way of felling, of touching, of loving. What you have caught on film is capture forever... is remembers little things after have forgotten everything. Wild life Photography powerful medium expression and communication, offers an infinite variety of perception, interpretation</p>
+
+                        <div className=" mt-4">
+                            <img src="https://demo.voidcoders.com/htmldemo/potoliaV2/main-files/assets/img/avatar/t-avatar-3.jpg" className='rounded-xl mx-auto' alt="" />
+                            <div className="ml-3 text-gray-300">
+                                <h5 className="text-xl font-semibold">Olivia  Martin</h5>
+                                <p className="font-semibold">Guest review</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
 
         </div>
     );
